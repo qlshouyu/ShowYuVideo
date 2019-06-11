a6714522a3dbeab1f136204dbd1bfcf6621fac8f
lugao2 <egojit@qq.com>
2019-06-11 23:10:46 +0800

    dev

---
+++ vshop/vshop-urms/vshop-urms-sso/src/main/java/com/qlshouyu/vshop/urms/sso/filters/CORSAuthenticationFilter.java
@@ -0,0 +1,10 @@
+package com.qlshouyu.vshop.urms.sso.filters;
+
+/**
+ * TODO
+ *
+ * @author 高露 邮箱：<a href="egojit@qq.com">egojit@qq.com</a>
+ * @since 2019-06-11 22:54
+ */
+public class CORSAuthenticationFilter {
+}
